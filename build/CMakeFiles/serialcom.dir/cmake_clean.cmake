1ba2347f043e33ba0945fd03ed0379096bf638f9
Gabriel Castro <gabriel1997.castro@gmail.com>
2019-03-14 17:51:48 -0300

    Add files

---
+++ build/CMakeFiles/serialcom.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/serialcom"
+  "CMakeFiles/serialcom-complete"
+  "serialcom-prefix/src/serialcom-stamp/serialcom-install"
+  "serialcom-prefix/src/serialcom-stamp/serialcom-mkdir"
+  "serialcom-prefix/src/serialcom-stamp/serialcom-download"
+  "serialcom-prefix/src/serialcom-stamp/serialcom-update"
+  "serialcom-prefix/src/serialcom-stamp/serialcom-patch"
+  "serialcom-prefix/src/serialcom-stamp/serialcom-configure"
+  "serialcom-prefix/src/serialcom-stamp/serialcom-build"
+  "serialcom-prefix/src/serialcom-stamp/serialcom-test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/serialcom.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/serialTest.cpp
@@ -0,0 +1,41 @@
+
+#include <serialcom.h>
+
+int main()
+{
+    SERIALPORTCONFIG serialPortConfig;
+    unsigned char* data;
+    int err;
+    unsigned char aux;
+
+    char command[] = {'#', '0', ' ', 'P', '1', '7', '0', '0', '\0'};
+
+    // Init the serial port
+    if((err = serialcom_init(&serialPortConfig, 1, "/dev/ttyS0", 115200)) != SERIALCOM_SUCCESS)
+    {
+        return err;
+    }
+
+    // Receive byte
+    if((err = serialcom_receivebyte(&serialPortConfig, data, 1e4)) != SERIALCOM_SUCCESS)
+    {
+        return err;
+    }
+
+    // Send the same byte
+    for (int i=0; i<8; i++)
+    {
+	aux = command[i];
+	data = &aux;
+	serialcom_sendbyte(&serialPortConfig, (unsigned char*) &data);
+    usleep(5);
+    }
+
+    // Close the serial port
+    if((err = serialcom_close(&serialPortConfig)) != SERIALCOM_SUCCESS)
+    {
+        return err;
+    }
+
+    return 0;
+}
